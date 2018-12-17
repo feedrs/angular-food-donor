24905d666463c1f2516925f702568816064171f2
Ikram <muhd.ikram.rosdi@gmail.com>
2018-12-17 21:41:40 +0800

    modified:   src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://food-donor.herokuapp.com',
+  serverUrl: 'https://food-donor.herokuapp.com/api',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
